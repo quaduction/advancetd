79a85da781e2b399c2654c6e5c8c84a14ccf56a7
kupofti <williambriand07@gmail.com>
2025-12-14 15:37:13 -0500

    I still have dementia though

--- project/core/Data/data.gd
+++ project/core/Data/data.gd
@@ -4,4 +4,7 @@ extends Node;
 @export var credits := 100;
 var towers: Dictionary = {}
 var towersOwned: Dictionary = {}
-var towersEquipped: Dictionary = {}
+var towersEquipped: Array = [];
+
+func _ready() -> void:
+	towersEquipped.resize(6);
