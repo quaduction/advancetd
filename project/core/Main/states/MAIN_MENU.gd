112bb1a7e94dd18f1d73aac201655e53bc1a0520
kupofti <williambriand07@gmail.com>
2025-12-13 21:30:04 -0500

    main menu can handoff

--- project/core/Main/states/MAIN_MENU.gd
+++ project/core/Main/states/MAIN_MENU.gd
@@ -5,3 +5,6 @@ var mainMenu;
 func load():
 	mainMenu = preload("res://ui/titleScreen/titleScreen.tscn").instantiate();
 	Game.menuManager.add_child(mainMenu);
+
+func handoff():
+	mainMenu.queue_free();
