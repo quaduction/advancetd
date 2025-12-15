991b98fe8bdff8787bc9e313acae228c9432571c
kupofti <williambriand07@gmail.com>
2025-12-14 19:48:15 -0500

    colour consts

--- project/core/Game/game.gd
+++ project/core/Game/game.gd
@@ -7,3 +7,11 @@ var states
 var levelManager: LevelManager;
 var menuManager: MenuManager;
 var data: Data;
+
+const consts := {
+	"highlight": {
+		"range": Color(0, 0, 1, 0.2),
+		"valid": Color(0, 1, 0, 0.5),
+		"invalid": Color(1, 0, 0, 0.5)
+	}
+};
