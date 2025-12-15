e9162129350a082edb6d38f25f9217cad37cc990
kupofti <williambriand07@gmail.com>
2025-12-15 11:19:59 -0500

    basic enemy movement and death

---
+++ project/entities/enemies/baseEnemy/base_enemy.gd
@@ -0,0 +1,47 @@
+extends PathFollow2D;
+
+
+var cashYield := 10;
+var hp := 10.0;
+var baseDamage := 5.0;
+var speed := 1.0;
+var isDestroyed := false;
+
+func _ready():
+	add_to_group("enemy");
+
+func _process(_delta):
+	#Move
+	progress_ratio += 0.0005 * speed
+	if progress_ratio == 1:
+		pathFinished()
+		return ;
+
+func pathFinished():
+	if isDestroyed: return ;
+	isDestroyed = true;
+
+	print("Damaged base for ", baseDamage);
+
+	queue_free();
+
+func takeDamage(damage):
+	if isDestroyed: return ;
+	
+	hp -= damage
+	damage_animation()
+	if hp <= 0:
+		isDestroyed = true;
+
+
+		queue_free();
+
+func damage_animation():
+	var tween := create_tween()
+	tween.tween_property(self, "v_offset", 0, 0.05)
+	tween.tween_property(self, "modulate", Color.ORANGE_RED, 0.1)
+	tween.tween_property(self, "modulate", Color.WHITE, 0.3)
+	tween.set_parallel()
+	tween.tween_property(self, "v_offset", -5, 0.2)
+	tween.set_parallel(false)
+	tween.tween_property(self, "v_offset", 0, 0.2)
