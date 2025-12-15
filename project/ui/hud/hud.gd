0183a6a468b01718ec4f97ad80ee08c4531769e0
kupofti <williambriand07@gmail.com>
2025-12-15 14:50:26 -0500

    labels & move

--- project/ui/hud/hud.gd
+++ project/ui/hud/hud.gd
@@ -56,8 +56,8 @@ func refresh_info_ui():
 	var hpLabel = $TopSheet/Row1/RightRow/HealthLabel;
 	var timeLabel = $TopSheet/Row1/RightRow/TimerLabel;
 
-	balLabel = Game.currentLevel.cash;
-	hpLabel = Game.currentLevel.baseHealth;
+	balLabel.text = "Balance: %d⋔" % Game.currentLevel.cash;
+	hpLabel.text = "Health: %d" % Game.currentLevel.baseHealth;
 
 #
 # Handlers
