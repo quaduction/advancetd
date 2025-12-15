68cf05ae3620a0190c42ba398a87063d534d5797
kupofti <williambriand07@gmail.com>
2025-12-15 00:34:16 -0500

    map 1 to debug for now

---
+++ project/ui/gameMenuScreen/game_menu_screen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_map_1_pressed() -> void:
+	Game.main.enterLevel("debugLevel");
+
+
+func _on_map_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_map_3_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_map_4_pressed() -> void:
+	pass # Replace with function body.
