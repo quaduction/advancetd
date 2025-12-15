497769472ab0f766efd3928d72e3402e88280ca0
LishunSLD <seanlarrydriesen@gmail.com>
2025-12-15 15:26:31 -0500

    shoot

--- project/entities/towers/projectileTower/projectile_tower.gd
+++ project/entities/towers/projectileTower/projectile_tower.gd
@@ -2,9 +2,11 @@ class_name ProjectileTower;
 extends Tower;
 
 @export var bullet := preload("./bullet/base_bullet.tscn");
+@onready var shootSfx := $shoot;
 
 func attack():
 	var projectile = bullet.instantiate();
 	projectile.init(self, currentTarget.position);
+	shootSfx.play()
 
 	Game.currentLevel.projectiles.add_child(projectile);
